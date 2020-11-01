1be29597326a42be4127849e627b92c744fd71cd
Filipp Ivkin <zloivoin4@gmail.com>
2020-11-01 21:47:56 +0300

    Library v2.0

---
+++ Search.java
@@ -0,0 +1,25 @@
+public class Search{ 
+public static int search (int[] Array, int searchNumber){
+	int searchIndex=-1;
+	int firstIndex=0;
+	int secondIndex=Array.length-1;
+	
+	while (firstIndex <= secondIndex)
+	{
+		int middleIndex = (firstIndex+secondIndex)/2;
+		if (Array[middleIndex]==searchNumber)
+		{
+			return middleIndex;
+		}
+		else if (Array[middleIndex]<searchNumber)
+		{
+			firstIndex=middleIndex+1;
+		}
+		else if (Array[middleIndex]>searchNumber)
+		{
+			secondIndex=middleIndex-1;
+		}
+	}
+	return -1;
+}
+}
